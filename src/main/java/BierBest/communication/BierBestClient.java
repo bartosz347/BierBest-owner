2901756da3a4593a8ce78e6e1fd1eff76ff171ec
Bartosz <bartosz347@gmail.com>
2017-05-23 19:45:54 +0200

    changed to SSL sockets

--- src/main/java/BierBest/communication/BierBestClient.java
+++ src/main/java/BierBest/communication/BierBestClient.java
@@ -1,21 +1,56 @@
 package BierBest.communication;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
 public class BierBestClient {
 
     public static final int PORT = 4488;
     public static final String SERVER_ADDRESS = "127.0.0.1";
 
-    public static void main(String[] args) {
-        System.out.printf("starting client");
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+
+        System.out.println("starting client");
+
         try {
-            Socket client = new Socket(SERVER_ADDRESS, PORT);
+
+            // Dumb trust manager that does not verify if we trust certificate's CA
+            final TrustManager[] trustAllCertificates = new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(final X509Certificate[] chain, final String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(final X509Certificate[] chain, final String authType) {
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            }};
+
+            SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+            sslContext.init(null, trustAllCertificates, null);
+
+
+            //Socket client = new Socket(SERVER_ADDRESS, PORT); // without SSL
+            //Socket client = SSLSocketFactory.getDefault().createSocket(SERVER_ADDRESS, PORT);
+            Socket client = sslContext.getSocketFactory().createSocket(SERVER_ADDRESS, PORT);
 
             System.out.println("connected to " + client.getRemoteSocketAddress());
-            ObjectOutputStream outToServer = new ObjectOutputStream(client.getOutputStream());
+            ObjectOutputStream outToServer = null;
+            try {
+                outToServer = new ObjectOutputStream(client.getOutputStream());
+            } catch (Exception e1) {
+
+            }
             ObjectInputStream inFromClient = new ObjectInputStream(client.getInputStream());
 
             outToServer.writeObject(new Message());
--- src/main/java/BierBest/communication/BierBestServer.java
+++ src/main/java/BierBest/communication/BierBestServer.java
@@ -1,19 +1,31 @@
 package BierBest.communication;
 
+import javax.net.ssl.SSLServerSocketFactory;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 
 public class BierBestServer {
 
     public static final int PORT = 4488;
 
     public static void main(String[] args) {
+
+        // PKCS#12 file containing certificate and private key
+        // setup here
+            //System.setProperty("javax.net.ssl.keyStore", "...");
+            //System.setProperty("javax.net.ssl.keyStorePassword", "...");
+        // or in VM options
+            //-Djavax.net.ssl.keyStorePassword="..." -Djavax.net.ssl.keyStore="..."
+
         System.out.println("starting server");
 
         try {
-            ServerSocket serverSocket = new ServerSocket(PORT);
+            //ServerSocket serverSocket = new ServerSocket(PORT); // without SSL
+            ServerSocket serverSocket = SSLServerSocketFactory.getDefault().createServerSocket(PORT);
+
             Socket clientSocket = serverSocket.accept();
 
             ObjectOutputStream outToClient = new ObjectOutputStream(clientSocket.getOutputStream());
